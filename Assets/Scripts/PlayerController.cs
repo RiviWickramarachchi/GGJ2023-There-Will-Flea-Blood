04a0d1999b9dfa37e1a9445f248621acd6197430
RiviWickramarachchi <riviwicky95@gmail.com>
2023-02-04 21:59:01 +0100

    movement with animation fixed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,12 +12,17 @@ public class PlayerController : MonoBehaviour
 	[SerializeField] private float jumpForce;
 	[SerializeField] private float slideOnLand;
 	[SerializeField] private InputActionAsset inputProvider;
-	[SerializeField] private float moveSpeed; //= 2f;
-
+	[SerializeField] private float moveSpeed = 6f;
+	[SerializeField] private float movementForce = 1f;
+	[SerializeField] private Camera playerCamera;
+	private InputAction move;
+	private Vector3 forceDirection = Vector3.zero;
 	private CharacterController controller;
-	private Vector3 playerVelocity;
 	private Vector2 moveVals;
+	private float maxSpeed = 20f;
 	private bool playerMovementLocked = false;
+	public float turnSmoothTime = 0.1f;
+	float turnSmoothVelocity;
     
 	public PlayerStates currentState;
 	public enum PlayerStates
@@ -58,6 +63,7 @@ public class PlayerController : MonoBehaviour
 
 	void OnEnable() {
 		inputProvider.FindAction("Directional Movements").Enable();
+		
 		inputProvider.FindAction("Suck Blood").Enable();
 		
 	}
@@ -72,9 +78,9 @@ public class PlayerController : MonoBehaviour
 
 	void FixedUpdate()
 	{
-		//player moves to the position where the seed is dropped
 		
 		MoveCharacter();
+		LookAt();
 	}
 
 	public void OnSpacePressed(InputAction.CallbackContext context)
@@ -95,6 +101,7 @@ public class PlayerController : MonoBehaviour
 		//get player input values
 		if(!playerMovementLocked)
         {
+
 			moveVals = context.ReadValue<Vector2>();
 			if (moveVals != Vector2.zero)
 			{
@@ -109,108 +116,127 @@ public class PlayerController : MonoBehaviour
 	private void MoveCharacter() {
 		if(!playerMovementLocked)
         {
-			//Get normalized directional vectors of the cameras
-			Vector3 camForward = Camera.main.transform.forward;
-			Vector3 camRight = Camera.main.transform.right;
-			camForward.y = 0;
-			camRight.y = 0;
-			camForward = camForward.normalized;
-			camRight = camRight.normalized;
 
-			//get direction-relative input vectors
-			Vector3 forwardRelativeVerticalInput = moveVals.y * camForward;
-			Vector3 rightRelativeVerticalInput = moveVals.x * camRight;
+			forceDirection += moveVals.x * GetCameraRight(playerCamera) *movementForce;
+			forceDirection += moveVals.y * GetCameraForward(playerCamera) * movementForce;
+			rb.AddForce(forceDirection, ForceMode.Impulse);
+			forceDirection = Vector3.zero;
 
-			//get camera relative movement vector
-			Vector3 camRelativeMovement = forwardRelativeVerticalInput + rightRelativeVerticalInput;
-			if (camRelativeMovement != Vector3.zero)
-			{
-				//Rotation is not necessary for the 2D sprites
-				//transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(camRelativeMovement), 0.15f);
-				transform.Translate(camRelativeMovement * moveSpeed * Time.deltaTime, Space.World);
-			}
-			Vector3 moveDir = Vector3.zero;
-			moveDir.x = moveVals.x;
-			moveDir.z = moveVals.y;
-			controller.Move(transform.TransformDirection(moveDir) * moveSpeed * Time.deltaTime);
+			Vector3 horizontalVelocity = rb.velocity;
+			horizontalVelocity.y = 0;
+			if (horizontalVelocity.sqrMagnitude > maxSpeed * maxSpeed)
+				rb.velocity = horizontalVelocity.normalized * maxSpeed + Vector3.up * rb.velocity.y;
+
 		}
 	}
 
-		/*
-		if (Input.GetMouseButtonDown(1))
-		{
-			TryJump();
+	private void LookAt()
+    {
+		Vector3 direction = rb.velocity;
+		direction.y = 0f;
+		if (moveVals.sqrMagnitude > 0.1f && direction.sqrMagnitude > 0.1f)
+        {
+			float targetAngle = Mathf.Atan2(moveVals.x, moveVals.y) * Mathf.Rad2Deg;
+			float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+			this.rb.rotation = Quaternion.Euler(0, angle, 0);
+
 		}
+			
+        else
+        {
+			rb.angularVelocity = Vector3.zero;
+        }
+    }
+
+	private Vector3 GetCameraForward(Camera playerCamera)
+    {
+		Vector3 forward = playerCamera.transform.forward;
+		forward.y = 0;
+		return forward.normalized;
+    }
+
+	private Vector3 GetCameraRight(Camera playerCamera)
+	{
+		Vector3 right = playerCamera.transform.right;
+		right.y = 0;
+		return right.normalized;
 	}
 
-	public void TryJump()
+	/*
+	if (Input.GetMouseButtonDown(1))
 	{
-		if (!_isGrounded) return;
-		Debug.Log("Do jump tried");
-		DoJump();
+		TryJump();
 	}
+}
 
-	private void DoJump()
-	{
-		_horizontalJumpVelocity = agent.velocity;
-		DisableNavMeshAgent();
-		EnableRB();
-		_isGrounded = false;
-		rb.AddForce(new Vector3(_horizontalJumpVelocity.x, jumpForce, _horizontalJumpVelocity.z), ForceMode.Impulse);
-	}
+public void TryJump()
+{
+	if (!_isGrounded) return;
+	Debug.Log("Do jump tried");
+	DoJump();
+}
 
-	private void SetUpRigidbody()
+private void DoJump()
+{
+	_horizontalJumpVelocity = agent.velocity;
+	DisableNavMeshAgent();
+	EnableRB();
+	_isGrounded = false;
+	rb.AddForce(new Vector3(_horizontalJumpVelocity.x, jumpForce, _horizontalJumpVelocity.z), ForceMode.Impulse);
+}
+
+private void SetUpRigidbody()
+{
+	rb = GetComponent<Rigidbody>();
+	if (rb == null)
 	{
+		gameObject.AddComponent<Rigidbody>();
 		rb = GetComponent<Rigidbody>();
-		if (rb == null)
-		{
-			gameObject.AddComponent<Rigidbody>();
-			rb = GetComponent<Rigidbody>();
-		}
+	}
+	DisableRB();
+	rb.freezeRotation = true;
+}
+
+// Effectively enables rigidbody.
+private void EnableRB()
+{
+	rb.isKinematic = false;
+	rb.useGravity = true;
+}
+
+// Effectively disables rigidbody. Technically still enabled but won't do much.
+private void DisableRB()
+{
+	rb.isKinematic = true;
+	rb.useGravity = false;
+}
+
+private void EnableNavMeshAgent()
+{
+	agent.Warp(transform.position);
+	if(Mathf.Abs(_horizontalJumpVelocity.x) > 0f || Mathf.Abs(_horizontalJumpVelocity.z) > 0f) agent.SetDestination(transform.position + (transform.forward * slideOnLand));
+	agent.updatePosition = true;
+	agent.updateRotation = true;
+	agent.isStopped = false;
+}
+
+private void DisableNavMeshAgent()
+{
+	agent.updatePosition = false;
+	agent.updateRotation = false;
+	agent.isStopped = true;
+}
+
+private void OnCollisionEnter(Collision other)
+{
+	Debug.Log(other.collider.tag);
+	if (other.collider != null && other.collider.tag == "Ground" && !_isGrounded && rb.velocity.y <= 0f)
+	{ // Condition assumes all ground is tagged with "Ground".
+		Debug.Log("Condition met!");
 		DisableRB();
-		rb.freezeRotation = true;
-	}
-
-	// Effectively enables rigidbody.
-	private void EnableRB()
-	{
-		rb.isKinematic = false;
-		rb.useGravity = true;
-	}
-
-	// Effectively disables rigidbody. Technically still enabled but won't do much.
-	private void DisableRB()
-	{
-		rb.isKinematic = true;
-		rb.useGravity = false;
-	}
-
-	private void EnableNavMeshAgent()
-	{
-		agent.Warp(transform.position);
-		if(Mathf.Abs(_horizontalJumpVelocity.x) > 0f || Mathf.Abs(_horizontalJumpVelocity.z) > 0f) agent.SetDestination(transform.position + (transform.forward * slideOnLand));
-		agent.updatePosition = true;
-		agent.updateRotation = true;
-		agent.isStopped = false;
-	}
-
-	private void DisableNavMeshAgent()
-	{
-		agent.updatePosition = false;
-		agent.updateRotation = false;
-		agent.isStopped = true;
-	}
-
-	private void OnCollisionEnter(Collision other)
-	{
-		Debug.Log(other.collider.tag);
-		if (other.collider != null && other.collider.tag == "Ground" && !_isGrounded && rb.velocity.y <= 0f)
-		{ // Condition assumes all ground is tagged with "Ground".
-			Debug.Log("Condition met!");
-			DisableRB();
-			EnableNavMeshAgent();
-		*/
-			//_isGrounded = true;
+		EnableNavMeshAgent();
+	*/
+	//_isGrounded = true;
 
 	void OnDisable() {
 		inputProvider.FindAction("Directional Movements").Disable();
